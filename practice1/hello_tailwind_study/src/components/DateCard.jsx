546362fee355c7a51b59938daa86de5a1d7ca7ed
sinjwu <harbirdouk@gmail.com>
2025-07-31 11:20:31 +0900

    250703sc

--- practice1/hello_tailwind_study/src/components/DateCard.jsx
+++ practice1/hello_tailwind_study/src/components/DateCard.jsx
@@ -1,12 +1,17 @@
-const DateCard = () => {
+const DateCard = ({ date, todos, isSat, isHoliday }) => {
   return (
     <div className="bg-white w-40 h-52 rounded-xl">
-      <div className="bg-red-400 text-2xl font-semibold text-right p-2 rounded-t-xl">
-        15
+      <div
+        className={`text-2xl font-semibold text-right p-2 rounded-t-xl bg-green-400 ${
+          isSat && "bg-orange-400"
+        } ${isHoliday && "bg-red-400"}`}
+      >
+        {date}
       </div>
       <div className="text-lg p-2 flex flex-col gap-2">
-        <p>광복절</p>
-        <p>청소하기</p>
+        {todos.map((todo, index) => (
+          <p key={index}>{todo}</p>
+        ))}
       </div>
     </div>
   );
