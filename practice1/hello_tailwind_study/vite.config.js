b82b9327d5a7d4153624e1402badd9ff14f295d3
sinjwu <harbirdouk@gmail.com>
2025-07-31 11:47:32 +0900

    250731fc

--- practice1/hello_tailwind_study/vite.config.js
+++ practice1/hello_tailwind_study/vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
+export default defineConfig(({ command, mode }) => {
+  if (typeof window !== "undefined") {
+    throw new Error(
+      "vite.config.js should not run in a browser-like environment."
+    );
+  }
+
+  return {
+    plugins: [react(), tailwindcss()],
+  };
 });
